2dc57e031f4a29b517e0362b112ebe7d03bd5fdc
fitia2023 <fitiandrianarisoa@gmail.com>
2023-06-21 16:23:36 +0400

    first back

---
+++ ProGest_back/src/test/java/mu/sil/progest_back/ProGestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.sil.progest_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProGestBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
